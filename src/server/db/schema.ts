727d778c5cee2fa493c3403c118ed53926660569
Carlos Lahrssen <carloslahrssen@gmail.com>
2024-03-22 04:30:34 +0000

    feat: first commit

---
+++ src/server/db/schema.ts
@@ -0,0 +1,24 @@
+// Example model schema from the Drizzle docs
+// https://orm.drizzle.team/docs/sql-schema-declaration
+
+import { sql } from "drizzle-orm";
+import {
+  index,
+  pgTableCreator,
+  serial,
+  text,
+  timestamp,
+} from "drizzle-orm/pg-core";
+
+export const createTable = pgTableCreator((name) => `z-t3-customer_${name}`);
+
+export const supportTickets = createTable(
+  "support_tickets",
+  {
+    id: serial("id").primaryKey(),
+    full_name: text('full_name'),
+    contactEmail: text('contact_email'),
+    problemDescription: text('problem_description'),
+    createdAt: timestamp('created_at').defaultNow()
+  }
+)
